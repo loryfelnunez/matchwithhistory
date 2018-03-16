cabec2fcc632074a1d3e5ea56a9474f3972f860e
loryn808 <loryn808@gmail.com>
2018-03-15 23:01:36 -0400

    Change SQL query for main page.

--- application.py
+++ application.py
@@ -25,7 +25,7 @@ cursor = conn.cursor()
 @application.route('/history', methods=['GET'])
 def history():
     cursor.execute ("""
-                SELECT distinct Main_Name FROM `scores_3` order by Main_Name where Score < .99
+                SELECT distinct Main_Name FROM `scores_3`  where Score < .99 order by Main_Name
                 """)
     response = cursor.fetchall()
     all_names = []
